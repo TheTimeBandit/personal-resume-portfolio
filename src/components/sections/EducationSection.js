0da8c46bbd97d466ec5b058ba94a1393862724b9
TheTimeBandit <tomixtt@gmail.com>
2020-08-05 16:37:15 +0200

    change external links to open in new browser tab

--- src/components/sections/EducationSection.js
+++ src/components/sections/EducationSection.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Section } from "../Section";
 import "./EducationSection.scss";
+import { ExternalLink } from "../shared/ExternalLink";
 
 export function EducationSection() {
   return (
     <Section id="Education" name="Education">
       <div className="left-border-separator mb-16">
         <div className="mb-16">
-          <a href="https://fmph.uniba.sk/">
+          <ExternalLink url="https://fmph.uniba.sk/">
             <h3>Faculty of Mathematics, Physics and Informatics, Bratislava</h3>
-          </a>
+          </ExternalLink>
           2016 - 2019 <br />
           Bachelor's degree in Applied Informatics
         </div>
 
         {/* <!-- TODO link to BP project detail page --> */}
-        <a href="">
-          <b>Bachelor thesis</b>
-        </a>
+        {/* <a href=""> */}
+        <b>Bachelor thesis</b>
+        {/* </a> */}
         <br />
         <div>
           Design and implementation of new frontend for a database exercise
---
+++ src/components/shared/ExternalLink.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function ExternalLink({ url, children, className }) {
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={className}
+    >
+      {children}
+    </a>
+  );
+}
